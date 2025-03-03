79e4a9ed08661f551fa06997d88ee8bda3e4ea95
fahmed121 <farazahmed1323@gmail.com>
2025-03-03 11:24:03 +0000

    Dockerfilecreation / azure
    created dockerfile and installed the azure service - currently not working

--- obj/Debug/net9.0/WebApplicationCoursework.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApplicationCoursework.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplicationCoursework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75cf63e7586131939ae779b0440d90fb3d1e58d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b67b4ce556ebcade1d6a983e804ee1ff2147561")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplicationCoursework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplicationCoursework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
